ddb6add844e6a1b4a4ed7ef7a082d6ae11e8871c
Brad Milne <brad.milne@getelevate.com>
2013-07-17 11:04:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cponce::Application.config.secret_token = 'cb91d23d080aee3f08d31cd90f684dc0205b695851323a33b3644a5e03f8ab4d78dd695f6fae4a181a6bc5d92ffedf361f76b6454e149b3a42122f8753be0bf8'
